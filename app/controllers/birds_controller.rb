0806ce1aa5b213d39d539cff27e0db8ab7ed6f2c
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-06-08 01:38:49 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,31 @@
-# Add code from Readme
+class BirdsController < ApplicationController
+
+def index
+    @birds=Bird.all
+end
+
+def new
+
+end
+
+def create
+
+end
+
+def edit
+
+end
+
+def update
+
+end
+
+def show
+
+end
+
+def destroy
+
+end
+
+end
